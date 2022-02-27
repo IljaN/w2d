fdf2fa13abd01a41591857315b985acb72b6fbc3
Ilja Neumann <node512@gmail.com>
2022-02-27 06:16:13 +0100

    Working translate

--- main.go
+++ main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/IljaN/w2d/deepl"
 	"github.com/alexflint/go-arg"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 type Config struct {
 	Article       string `arg:"required,positional"`
 	TargetLang    string `arg:"-l,--" help:"target language for translation"`
 	WikExecutable string `arg:"-w,--" help:"Path to the wik executable" default:"/usr/local/bin/wik"`
-	DeeplAuthKey  string `arg:"env:DEEPL_AUTH_KEY"`
+	DeeplAuthKey  string `arg:"required,--,env:DEEPL_AUTH_KEY"`
 }
 
 func loadConfig() *Config {
 	c := Config{}
 	arg.MustParse(&c)
+	c.DeeplAuthKey = strings.TrimSpace(c.DeeplAuthKey)
+
 	return &c
 }
 
@@ -24,6 +28,22 @@ func fetchArticle(cfg *Config) (text []byte, err error) {
 	termFlag := fmt.Sprintf("-s %s", cfg.Article)
 	cmd := exec.Command(cfg.WikExecutable, termFlag)
 	return cmd.Output()
+}
+
+func translate(cfg *Config, text string) (string, error) {
+	dc := deepl.NewClient(cfg.DeeplAuthKey)
+	translatedSentences, err := dc.Translate(text, "RU", "")
+	if err != nil {
+		return "", err
+	}
+
+	translatedText := ""
+	for numSentence := range translatedSentences {
+		translatedText = translatedText + translatedSentences[numSentence]
+	}
+
+	return translatedText, err
+
 }
 
 func main() {
@@ -34,7 +54,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(txt)
+	translated, err := translate(cfg, string(txt))
+	fmt.Println(translated)
+
 	os.Exit(0)
 
 }
