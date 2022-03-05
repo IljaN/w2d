c04fa11fe3ea56dbeb15a9ed4303b687536b3c65
Ilja Neumann <node512@gmail.com>
2022-03-05 16:57:44 +0100

    Extracted Source, simplified main

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"github.com/IljaN/w2d/deepl"
 	"github.com/IljaN/w2d/wikipedia"
 	"github.com/alexflint/go-arg"
-	"io"
-	"net/http"
 	"os"
 )
 
@@ -21,7 +19,7 @@ func (Config) Description() string {
 }
 
 type translateCmd struct {
-	Article    string `arg:"positional,required" help:"full url to the article which should be translated"`
+	Article    Source `arg:"positional" default:"-" help:"full url to the article which should be translated"`
 	TargetLang string `arg:"-t,--" default:"en" help:"target language for translation"`
 	SourceLang string `arg:"-s,--" default:"" help:"source language, leave empty for autodetect"`
 	ParseOnly  bool   `arg:"-p,--" default:"false" help:"parse to markdown without translating"`
@@ -36,13 +34,18 @@ type listLangsCmd struct {
 // w2d - translates wikipedia articles using DeepL api and renders them to markdown.
 func main() {
 	c := Config{}
-	arg.MustParse(&c)
+	p := arg.MustParse(&c)
+
 	switch {
 	case c.ListLangs != nil:
 		c.ListLangs.authKey = c.DeeplAuthKey
 		listLanguages(c.ListLangs)
 	case c.Translate != nil:
 		c.Translate.authKey = c.DeeplAuthKey
+		if !c.Translate.Article.StdIn && !c.Translate.Article.IsUrl {
+			p.Fail("article is required")
+
+		}
 		translate(c.Translate)
 	}
 
@@ -51,13 +54,13 @@ func main() {
 
 // translate fetches an article from wikipedia, parses to markdown and translates it using DeepL
 func translate(cfg *translateCmd) {
-	article, err := fetch(cfg)
+	article, err := cfg.Article.Reader()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	markdown, err := parse(cfg, article)
+	markdown, err := wikipedia.NewArticleParser().Parse(article)
 	if err != nil {
 		fmt.Printf("failed to parse: %s", err)
 		os.Exit(2)
@@ -77,21 +80,6 @@ func translate(cfg *translateCmd) {
 	fmt.Print(translated)
 	os.Exit(0)
 
-}
-
-// fetch downloads html from a given url
-func fetch(cfg *translateCmd) (io.Reader, error) {
-	resp, err := http.Get(cfg.Article)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp.Body, err
-}
-
-// parse converts article to markdown
-func parse(cfg *translateCmd, article io.Reader) (text string, err error) {
-	return wikipedia.NewArticleParser().Parse(article)
 }
 
 // translateArticle translates text using DeepL API
